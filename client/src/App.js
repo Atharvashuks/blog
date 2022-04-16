1fa20b8dc445a01513b5d45d577d9dc7f19448d9
Atharvashuks <atharvashuklaas@gmail.com>
2022-04-16 23:35:31 +0530

    redux setup

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import { Navbar } from "./components";
-import { Header, Single, Write, Settings, Login, Register,Home } from "./container";
+import { Single, Write, Settings, Login, Register, Home } from "./container";
 
 const App = () => {
   const user = true;
@@ -11,12 +11,20 @@ const App = () => {
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" exact element={<Home />}/>
-        <Route path="/register" exact element={user ? <Home /> : <Register />}/>
-        <Route path="/login" exact element={user ? <Home /> :<Login />}/>
-        <Route path="/write" exact element={user ? <Write /> : <Register />}/>
-        <Route path="/settings" exact element={user ? <Settings /> : <Register />}/>
-        <Route path="/post/:postId" exact element={<Single />}/>
+        <Route path="/" exact element={<Home />} />
+        <Route
+          path="/register"
+          exact
+          element={user ? <Home /> : <Register />}
+        />
+        <Route path="/login" exact element={user ? <Home /> : <Login />} />
+        <Route path="/write" exact element={user ? <Write /> : <Register />} />
+        <Route
+          path="/settings"
+          exact
+          element={user ? <Settings /> : <Register />}
+        />
+        <Route path="/post/:postId" exact element={<Single />} />
       </Routes>
     </BrowserRouter>
   );
--- client/src/container/Header/Header.jsx
+++ client/src/container/Header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
   return (
     <div className="header">
         <div className="header__header-titles">
-            <span className="header__header-title-small">React & Node</span>
+            <span className="header__header-title-small">Write your Mind!!</span>
             <span className="header__header-title-large">Blogs</span>
         </div>
         <img className="header__img" src={images.background2} alt="background" />
